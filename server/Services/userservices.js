80cc419557f1c179c6fb757de258c5d692ee9250
amnaqamar123 <amnak3824@gmail.com>
2025-12-01 15:56:10 +0500

    added successfully

--- server/Services/userservices.js
+++
@@ -1,9 +0,0 @@
-const {UserSchema}=require('../Model/User')
-
-const findUserByemail=async(email)=>{
-    const user=await UserSchema.findOne({email})
-    return user
-
-}
-
-module.exports={findUserByemail}
